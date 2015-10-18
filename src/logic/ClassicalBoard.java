08c7297a02de964d611eba91ba3126354fd76df2
Alexey Kozhevnikov <kurtzhaar@mail.ru>
2015-10-19 00:16:06 +0300

    Дописан список полей для классической монополии.

--- src/logic/ClassicalBoard.java
+++ src/logic/ClassicalBoard.java
@@ -26,18 +26,35 @@ public class ClassicalBoard {
         fields[9] = ClassicField.Connecticut;
         fields[10] = ClassicField.JustVisitingJail;
         fields[11] = ClassicField.StCharles;
-        fields[12] = ClassicField.States;
-        fields[13] = ClassicField.Virginia;
-        fields[14] = ClassicField.PennsylvaniaRailroad;
-        fields[15] = ClassicField.;
-        fields[16] = ClassicField.CommunityChest1;
-        fields[17] = ClassicField.Mediterannean;
-        fields[18] = ClassicField.CommunityChest1;
-        fields[19] = ClassicField.Mediterannean;
-        fields[20] = ClassicField.CommunityChest1;
-
-
-
+        fields[12] = ClassicField.ElectricCompany;
+        fields[13] = ClassicField.States;
+        fields[14] = ClassicField.Virginia;
+        fields[15] = ClassicField.PennsylvaniaRailroad;
+        fields[16] = ClassicField.StJames;
+        fields[17] = ClassicField.CommunityChest2;
+        fields[18] = ClassicField.Tennessee;
+        fields[19] = ClassicField.NewYork;
+        fields[20] = ClassicField.FreeParking;
+        fields[21] = ClassicField.Kentucky;
+        fields[22] = ClassicField.Chance2;
+        fields[23] = ClassicField.Indiana;
+        fields[24] = ClassicField.Illinois;
+        fields[25] = ClassicField.BORailroad;
+        fields[26] = ClassicField.Atlantic;
+        fields[27] = ClassicField.Ventnor;
+        fields[28] = ClassicField.WaterWorks;
+        fields[29] = ClassicField.MarvinGardens;
+        fields[30] = ClassicField.GoToJail;
+        fields[31] = ClassicField.Pacific;
+        fields[32] = ClassicField.NorthCarolina;
+        fields[33] = ClassicField.CommunityChest3;
+        fields[34] = ClassicField.Pennsylvania;
+        fields[35] = ClassicField.ShortLineRailroad;
+        fields[36] = ClassicField.Chance3;
+        fields[37] = ClassicField.ParkPlace;
+        fields[38] = ClassicField.LuxuryTax;
+        fields[39] = ClassicField.BroadWalk;
+        fields[40] = ClassicField.Jail;
     }
 
     public ClassicField[] getFields() {
